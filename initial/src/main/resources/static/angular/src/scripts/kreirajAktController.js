133c65389143dfbcd7506ac071f77bba55016529
vuletic <vuletic93@gmail.com>
2016-06-09 19:32:54 +0200

    Amandman se unosi u bazu

--- initial/src/main/resources/static/angular/src/scripts/kreirajAktController.js
+++ initial/src/main/resources/static/angular/src/scripts/kreirajAktController.js
@@ -4,8 +4,63 @@
 module.exports = [
     '$scope', '$http',
     function ctrl($scope){
-        
-        var xml="<Amandman Id=''><Podnosilac></Podnosilac><ElementAmandmana Akcija='Dodaj' Referencira=''></ElementAmandmana></Amandman>";
+        var docSpec={
+            onchange: function(){
+                console.log("I been changed now!")
+            },
+            validate: function(obj){
+                console.log("I be validatin' now!")
+            },
+            elements: {
+                "Amandman": {
+                    menu: [{
+                        caption: "Dodaj <Podnosilac>",
+                        action: Xonomy.newElementChild,
+                        actionParameter: "<Podnosilac/>"
+                    },
+                        {
+                            caption: "Dodaj <ElementAmandman>",
+                            action: Xonomy.newElementChild,
+                            actionParameter: "<Podnosilac/>"
+                        }
+                    
+                    ]
+                },
+                "Podnosilac": {
+                    menu: [{
+                        caption: "Add @label=\"something\"",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "label", value: "something"},
+                        hideIf: function(jsElement){
+                            return jsElement.hasAttribute("label");
+                        }
+                    }, {
+                        caption: "Delete this <item>",
+                        action: Xonomy.deleteElement
+                    }, {
+                        caption: "New <item> before this",
+                        action: Xonomy.newElementBefore,
+                        actionParameter: "<item/>"
+                    }, {
+                        caption: "New <item> after this",
+                        action: Xonomy.newElementAfter,
+                        actionParameter: "<item/>"
+                    }],
+                    canDropTo: ["list"],
+                    attributes: {
+                        "label": {
+                            asker: Xonomy.askString,
+                            menu: [{
+                                caption: "Delete this @label",
+                                action: Xonomy.deleteAttribute
+                            }]
+                        }
+                    }
+                }
+            }
+        };
+
+        var xml="<Amandman><Podnosilac></Podnosilac><ElementAmandmana Akcija='Dodaj' Referencira=''></ElementAmandmana></Amandman>";
         var editor=document.getElementById("editor");
         Xonomy.setMode("laic");
         Xonomy.render(xml, editor, docSpec);
