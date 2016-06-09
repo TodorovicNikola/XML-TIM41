22df4a04bf0604eef87d3af1b334c2d2c46db1c6
nenadtod <nenadtod@live.com>
2016-06-09 21:26:54 +0200

    Editor za akt skoro skroz (treba skontati sta cemo sa ReferenceUri :)). Obrisan onaj jquery.xmleditor.

--- initial/src/main/resources/static/angular/src/scripts/kreirajAktController.js
+++ initial/src/main/resources/static/angular/src/scripts/kreirajAktController.js
@@ -23,9 +23,45 @@ module.exports = [
                             {
                                 caption: "Dodaj <Deo>",
                                 action: Xonomy.newElementChild,
-                                actionParameter: "<Deo/>"
+                                actionParameter: "<Deo Id=''/>"
+                            },
+                            {
+                                caption: "Dodaj @Naslov",
+                                action: Xonomy.newAttribute,
+                                actionParameter: {name: "Naslov", value: ""},
+                                hideIf: function(jsElement) {
+                                    return jsElement.hasAttribute("Naslov");
+                                }
+                            },
+                            {
+                                caption: "Dodaj @Redni @broj",
+                                action: Xonomy.newAttribute,
+                                actionParameter: {name: "Redni broj", value: ""},
+                                hideIf: function(jsElement){
+                                    return jsElement.hasAttribute("Redni broj");
+                                }
                             }
-                    ]
+                    ],
+                    attributes:{
+                        "Id":{
+                            asker: Xonomy.askString
+                        },
+                        "Naslov":{
+                            asker: Xonomy.askString
+                        },
+                        "Redni broj":{
+                            asker: Xonomy.askString
+                        },
+                        "DatumPodnosenja":{
+                            asker: Xonomy.askString
+                        },
+                        "Status": {
+                            asker: Xonomy.askPicklist,
+                            askerParameter: [
+                                "Usvojen", "U proceduri", "Odbijen"
+                            ]
+                        }
+                    }
                 },
                 "Podnosilac":{
                     hasText: true,
@@ -51,25 +87,88 @@ module.exports = [
                         action: Xonomy.newElementChild,
                         actionParameter: "<Glava/>"
 
-                    },{
+                    },
+                    {
+                        caption: "Dodaj @Naslov",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Naslov", value: ""},
+                        hideIf: function(jsElement) {
+                            return jsElement.hasAttribute("Naslov");
+                        }
+                    },
+                    {
+                        caption: "Dodaj @Redni @broj",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Redni broj", value: ""},
+                        hideIf: function(jsElement){
+                            return jsElement.hasAttribute("Redni broj");
+                        }
+                    },
+                    {
                         caption: "Obriši",
                         action: Xonomy.deleteElement
                     }],
-                    mustBeAfter: ["Podnosilac"]
+                    mustBeAfter: ["Podnosilac"],
+                    attributes:{
+                        "Id":{
+                            asker: Xonomy.askString
+                        },
+                        "Naslov":{
+                            asker: Xonomy.askString
+                        },
+                        "Redni broj":{
+                            asker: Xonomy.askString
+                        }
+                    }
                 },
                 "Glava":{
                     menu: [{
+                        caption: "Dodaj novi <Glava>",
+                        action: Xonomy.newElementAfter,
+                        actionParameter: "<Glava/>"
+
+                    },{
                         caption: "Dodaj novi <Odeljak>",
                         action: Xonomy.newElementChild,
                         actionParameter: "<Odeljak/>"
 
                     },{
+                        caption: "Dodaj @Naslov",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Naslov", value: ""},
+                        hideIf: function(jsElement) {
+                            return jsElement.hasAttribute("Naslov");
+                        }
+                    },{
+                            caption: "Dodaj @Redni @broj",
+                            action: Xonomy.newAttribute,
+                            actionParameter: {name: "Redni broj", value: ""},
+                            hideIf: function(jsElement){
+                                return jsElement.hasAttribute("Redni broj");
+                            }
+                    },{
                         caption: "Obriši",
                         action: Xonomy.deleteElement
-                    }]
+                    }],
+                    attributes:{
+                        "Id":{
+                            asker: Xonomy.askString
+                        },
+                        "Naslov":{
+                            asker: Xonomy.askString
+                        },
+                        "Redni broj":{
+                            asker: Xonomy.askString
+                        }
+                    }
                 },
                 "Odeljak":{
                     menu: [{
+                        caption: "Dodaj novi <Odeljak>",
+                        action: Xonomy.newElementAfter,
+                        actionParameter: "<Odeljak/>"
+
+                    },{
                         caption: "Dodaj novi <Pododeljak>",
                         action: Xonomy.newElementChild,
                         actionParameter: "<Pododeljak/>",
@@ -84,25 +183,85 @@ module.exports = [
                             return jsElement.hasChildElement("Pododeljak");
                         }
                     },{
+                        caption: "Dodaj @Naslov",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Naslov", value: ""},
+                        hideIf: function(jsElement) {
+                            return jsElement.hasAttribute("Naslov");
+                        }
+                    },{
+                        caption: "Dodaj @Redni @broj",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Redni broj", value: ""},
+                        hideIf: function(jsElement){
+                            return jsElement.hasAttribute("Redni broj");
+                        }
+                    },{
                         caption: "Obriši",
                         action: Xonomy.deleteElement
-                    }]
+                    }],
+                    attributes:{
+                        "Id":{
+                            asker: Xonomy.askString
+                        },
+                        "Naslov":{
+                            asker: Xonomy.askString
+                        },
+                        "Redni broj":{
+                            asker: Xonomy.askString
+                        }
+                    }
 
                 },
                 "Pododeljak":{
 
                     menu: [{
+                        caption: "Dodaj novi <Pododeljak>",
+                        action: Xonomy.newElementAfter,
+                        actionParameter: "<Pododeljak/>"
+
+                    },{
                         caption: "Dodaj novi <Clan>",
                         action: Xonomy.newElementChild,
                         actionParameter: "<Clan/>"
                     },{
+                        caption: "Dodaj @Naslov",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Naslov", value: ""},
+                        hideIf: function(jsElement) {
+                            return jsElement.hasAttribute("Naslov");
+                        }
+                    },{
+                        caption: "Dodaj @Redni @broj",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Redni broj", value: ""},
+                        hideIf: function(jsElement){
+                            return jsElement.hasAttribute("Redni broj");
+                        }
+                    },{
                         caption: "Obriši",
                         action: Xonomy.deleteElement
-                    }]
+                    }],
+                    attributes:{
+                        "Id":{
+                            asker: Xonomy.askString
+                        },
+                        "Naslov":{
+                            asker: Xonomy.askString
+                        },
+                        "Redni broj":{
+                            asker: Xonomy.askString
+                        }
+                    }
 
                 },
                 "Clan":{
                     menu: [{
+                        caption: "Dodaj novi <Clan>",
+                        action: Xonomy.newElementAfter,
+                        actionParameter: "<Clan/>"
+
+                    },{
                         caption: "Dodaj novi <Stav>",
                         action: Xonomy.newElementChild,
                         actionParameter: "<Stav/>"
@@ -111,12 +270,42 @@ module.exports = [
                         action: Xonomy.newElementChild,
                         actionParameter: "<Sadrzaj/>"
                     },{
+                        caption: "Dodaj @Naslov",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Naslov", value: ""},
+                        hideIf: function(jsElement) {
+                            return jsElement.hasAttribute("Naslov");
+                        }
+                    },{
+                        caption: "Dodaj @Redni @broj",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Redni broj", value: ""},
+                        hideIf: function(jsElement){
+                            return jsElement.hasAttribute("Redni broj");
+                        }
+                    },{
                         caption: "Obriši",
                         action: Xonomy.deleteElement
-                    }]
+                    }],
+                    attributes:{
+                        "Id":{
+                            asker: Xonomy.askString
+                        },
+                        "Naslov":{
+                            asker: Xonomy.askString
+                        },
+                        "Redni broj":{
+                            asker: Xonomy.askString
+                        }
+                    }
                 },
                 "Stav":{
                     menu: [{
+                        caption: "Dodaj novi <Stav>",
+                        action: Xonomy.newElementAfter,
+                        actionParameter: "<Stav/>"
+
+                    },{
                         caption: "Dodaj novi <Tacka>",
                         action: Xonomy.newElementChild,
                         actionParameter: "<Tacka/>"
@@ -125,12 +314,42 @@ module.exports = [
                         action: Xonomy.newElementChild,
                         actionParameter: "<Sadrzaj/>"
                     },{
+                        caption: "Dodaj @Naslov",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Naslov", value: ""},
+                        hideIf: function(jsElement) {
+                            return jsElement.hasAttribute("Naslov");
+                        }
+                    },{
+                        caption: "Dodaj @Redni @broj",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Redni broj", value: ""},
+                        hideIf: function(jsElement){
+                            return jsElement.hasAttribute("Redni broj");
+                        }
+                    },{
                         caption: "Obriši",
                         action: Xonomy.deleteElement
-                    }]
+                    }],
+                    attributes:{
+                        "Id":{
+                            asker: Xonomy.askString
+                        },
+                        "Naslov":{
+                            asker: Xonomy.askString
+                        },
+                        "Redni broj":{
+                            asker: Xonomy.askString
+                        }
+                    }
                 },
                 "Tacka":{
                     menu: [{
+                        caption: "Dodaj novi <Tacka>",
+                        action: Xonomy.newElementAfter,
+                        actionParameter: "<Tacka/>"
+
+                    },{
                         caption: "Dodaj novi <Podtacka>",
                         action: Xonomy.newElementChild,
                         actionParameter: "<Podtacka/>"
@@ -139,12 +358,42 @@ module.exports = [
                         action: Xonomy.newElementChild,
                         actionParameter: "<Sadrzaj/>"
                     },{
+                        caption: "Dodaj @Naslov",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Naslov", value: ""},
+                        hideIf: function(jsElement) {
+                            return jsElement.hasAttribute("Naslov");
+                        }
+                    },{
+                        caption: "Dodaj @Redni @broj",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Redni broj", value: ""},
+                        hideIf: function(jsElement){
+                            return jsElement.hasAttribute("Redni broj");
+                        }
+                    },{
                         caption: "Obriši",
                         action: Xonomy.deleteElement
-                    }]
+                    }],
+                    attributes:{
+                        "Id":{
+                            asker: Xonomy.askString
+                        },
+                        "Naslov":{
+                            asker: Xonomy.askString
+                        },
+                        "Redni broj":{
+                            asker: Xonomy.askString
+                        }
+                    }
                 },
                 "Podtacka":{
                     menu: [{
+                        caption: "Dodaj novi <Podtacka>",
+                        action: Xonomy.newElementAfter,
+                        actionParameter: "<Podtacka/>"
+
+                    },{
                         caption: "Dodaj novi <Alineja>",
                         action: Xonomy.newElementChild,
                         actionParameter: "<Alineja/>"
@@ -153,25 +402,49 @@ module.exports = [
                         action: Xonomy.newElementChild,
                         actionParameter: "<Sadrzaj/>"
                     },{
+                        caption: "Dodaj @Naslov",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Naslov", value: ""},
+                        hideIf: function(jsElement) {
+                            return jsElement.hasAttribute("Naslov");
+                        }
+                    },{
+                        caption: "Dodaj @Redni @broj",
+                        action: Xonomy.newAttribute,
+                        actionParameter: {name: "Redni broj", value: ""},
+                        hideIf: function(jsElement){
+                            return jsElement.hasAttribute("Redni broj");
+                        }
+                    },{
                         caption: "Obriši",
                         action: Xonomy.deleteElement
-                    }]
+                    }],
+                    attributes:{
+                        "Id":{
+                            asker: Xonomy.askString
+                        },
+                        "Naslov":{
+                            asker: Xonomy.askString
+                        },
+                        "Redni broj":{
+                            asker: Xonomy.askString
+                        }
+                    }
                 },
                 "Alineja":{
                     hasText: true,
                     inlineMenu: [{
                         caption: "Wrap with <Referenca>",
                         action: Xonomy.wrap,
-                        actionParameter: {template: "<Referenca>$</Referenca>", placeholder: "$"}
+                        actionParameter: {template: "<Referenca ReferencaURI=''>$</Referenca>", placeholder: "$"}
                     }]
-
                 },
                 "Sadrzaj":{
                     hasText: true,
                     inlineMenu: [{
                         caption: "Wrap with <Referenca>",
                         action: Xonomy.wrap,
-                        actionParameter: {template: "<Referenca>$</Referenca>", placeholder: "$"}
+                        actionParameter: {template: "<Referenca ReferencaURI=''>$</Referenca>", placeholder: "$"}
                     }]
                 }
             }
@@ -179,7 +452,7 @@ module.exports = [
         };
 
 
-        var xml="<Akt><Podnosilac></Podnosilac><Deo></Deo></Akt>";
+        var xml="<Akt Id='' DatumPodnosenja='' Status='U proceduri'><Podnosilac></Podnosilac><Deo></Deo></Akt>";
         var editor=document.getElementById("editor");
         Xonomy.setMode("laic");
         Xonomy.render(xml, editor, docSpec);
