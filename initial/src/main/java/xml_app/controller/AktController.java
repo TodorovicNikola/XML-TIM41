86decb78df551f10f06dfb655db190db54336a46
nenadtod <nenadtod@live.com>
2016-06-11 19:59:04 +0200

    Odradjen deo rekurzije. Moram da commit da bih pull:P

--- initial/src/main/java/xml_app/controller/AktController.java
+++ initial/src/main/java/xml_app/controller/AktController.java
@@ -1,9 +1,15 @@
 package xml_app.controller;
 
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import xml_app.database.DatabaseHelper;
 import xml_app.model.Akt;
 import xml_app.model.Amandman;
+import java.util.Hashtable;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.bind.JAXBContext;
@@ -11,15 +17,20 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.util.JAXBSource;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.StringReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -80,7 +91,7 @@ public class AktController {
     @RequestMapping(value = "/dodaj",method = RequestMethod.POST)
     public Akt trial(@RequestBody String telo) throws JAXBException {
 
-        JAXBContext jaxbContext = JAXBContext.newInstance(Akt.class);
+        /*JAXBContext jaxbContext = JAXBContext.newInstance(Akt.class);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 
         StringReader reader = new StringReader(telo);
@@ -94,11 +105,80 @@ public class AktController {
             return a;
         }catch(Exception e){
             e.printStackTrace();
+        }*/
+
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(new InputSource(new StringReader(telo)));
+            doc.getDocumentElement().normalize();
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
 
         return null;
 
 
+    }
+
+    private void fillInIds(Node node){
+
+        Hashtable<String, Integer> namesCount = new Hashtable<String, Integer>();
+
+        if(node.getNodeType() != Node.ELEMENT_NODE)
+            return;
+
+        NodeList childNodes = node.getChildNodes();
+        for (int i=0; i < childNodes.getLength(); i++) {
+            Node subnode = childNodes.item(i);
+            if (subnode.getNodeType() == Node.ELEMENT_NODE) {
+
+                int count;
+                String nameKey =  subnode.getNodeName();
+                if(namesCount.contains(nameKey)){
+                    count = namesCount.get(nameKey);
+                    Integer val = namesCount.get(nameKey);
+                    val ++;
+                }else{
+                    count = 1;
+                    namesCount.put(nameKey, count);
+                }
+
+                Node idNode = findIdNode("Id", subnode);
+
+
+
+
+            }
+        }
+
+    }
+
+
+    //Funkcija kojoj se prosledi node, i ime child nodova koji se traze
+    //Vraca listu child nodova
+    private Node findIdNode(String name, Node node) {
+        if (node.getNodeType() != Node.ELEMENT_NODE) {
+            return null;
+        }
+
+        if (! node.hasChildNodes()) return null;
+
+        NodeList list = node.getChildNodes();
+        for (int i=0; i < list.getLength(); i++) {
+            Node subnode = list.item(i);
+            if (subnode.getNodeType() == Node.ATTRIBUTE_NODE) {
+                if (subnode.getNodeName().equals(name))
+                    return subnode;
+            }
+        }
+        return null;
     }
 
 }
