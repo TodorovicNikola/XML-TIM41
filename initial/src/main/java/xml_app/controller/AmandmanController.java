c341c228244928373afdd920304084a61ad9ddcb
Aleksa_Mirkovic <aleksa.d.mirkovic@gmail.com>
2016-06-13 12:40:18 +0200

    Amandman xsl fo dodat(par korekcija potrebnih oko referenci), u AmandmanControlleru  napravljeno generisanje pdfa za amandman, api/amandmani/1/pdf.

--- initial/src/main/java/xml_app/controller/AmandmanController.java
+++ initial/src/main/java/xml_app/controller/AmandmanController.java
@@ -1,9 +1,16 @@
 package xml_app.controller;
 
+import net.sf.saxon.TransformerFactoryImpl;
 import org.apache.catalina.connector.Response;
+import org.apache.fop.apps.FOPException;
+import org.apache.fop.apps.FOUserAgent;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopFactory;
+import org.apache.xmlgraphics.util.MimeConstants;
 import org.springframework.web.bind.annotation.*;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import xml_app.database.DatabaseHelper;
 import xml_app.model.Akt;
 import xml_app.model.Amandman;
@@ -22,15 +29,13 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.StringReader;
+import java.io.*;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -108,6 +113,63 @@ public class AmandmanController {
         }
 
 
+    }
+
+    @RequestMapping(value = "/{amandmanId}/pdf",method = RequestMethod.GET)
+    public void konkretanAmandmanPdf(@PathVariable String amandmanId, HttpServletResponse resp) throws IOException {
+        //trebalo bi ovo wrapovati u neku metodu, ali za sada neka ostane tako
+        DatabaseHelper db = new DatabaseHelper();
+        Amandman a = db.findAmandmanById(amandmanId);
+        JAXBSource source = null;
+        FopFactory fopFactory = null;
+        TransformerFactory transformerFactory;
+        db.release();
+        try {
+            JAXBContext jc = JAXBContext.newInstance(Amandman.class);
+            source = new JAXBSource(jc, a);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            fopFactory = FopFactory.newInstance(new File("XSDs/fop.xconf"));
+            //konf fajl za prikaz, za sada je to onaj sa vezbi.moze se customizovati malo
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        transformerFactory = new TransformerFactoryImpl();
+        File xsltFile = new File("XSDs/Amandman_xsl_fo.xsl");
+        StreamSource transformSource = new StreamSource(xsltFile);
+
+
+        FOUserAgent userAgent = fopFactory.newFOUserAgent();
+
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+
+
+        Transformer xslFoTransformer = null;
+        try {
+            xslFoTransformer = transformerFactory.newTransformer(transformSource);
+            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, userAgent, outStream);
+
+            Result res = new SAXResult(fop.getDefaultHandler());
+
+            xslFoTransformer.transform(source, res);
+            // u sax result storuje
+            //u outStream su bajti potrebni za pustanje kroz izlazni bafer
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (FOPException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+        System.out.println(a.getDatumIVremePodnosenja());
+        resp.getOutputStream().write(outStream.toByteArray());
+        System.out.println("Outputovan pdf");
     }
 
 }
