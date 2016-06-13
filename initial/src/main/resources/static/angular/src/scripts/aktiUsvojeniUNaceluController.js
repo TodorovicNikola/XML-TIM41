c6e8fe0591dd45d70f1ce086ff7df8333b2db2f5
nkl58 <nikola5tod@yahoo.com>
2016-06-13 12:15:02 +0200

    fixed the brljotinu izazvanu merge-om, dunno how it happened

---
+++ initial/src/main/resources/static/angular/src/scripts/aktiUsvojeniUNaceluController.js
@@ -0,0 +1,56 @@
+module.exports = [
+        '$scope', '$http', '$routeParams',
+
+    function myController($scope, $http, $routeParams){
+        $http.get("/api/akti/u-nacelu").then(function(response) {
+            $scope.data = response.data;
+
+        });
+
+
+        $http({
+            method: "Get",
+            url: "api/state/getState",
+            dataType: "json",
+            traditional:true
+        }).then(function (response) {
+            $scope.state = response.data.data;
+
+        });
+
+
+        $scope.glasoviZa;
+        $scope.glasoviProtiv;
+        $scope.glasoviUzdrzani;
+        $scope.idAkta;
+        $scope.naslov;
+
+        var data = {};
+
+
+
+        $scope.setParams = function(id, naslov){
+            $scope.idAkta = id;
+            $scope.naslov = naslov;
+
+        }
+
+        $scope.submitVotes = function(){
+            data = { idAkta: $scope.idAkta, glasoviZa: $scope.glasoviZa, glasoviProtiv: $scope.glasoviProtiv, glasoviUzdrzani: $scope.glasoviUzdrzani };
+
+            $http({
+                method: "Post",
+                url: "api/vote/voteUCelosti",
+                data: data,
+                dataType: "json",
+                traditional:true
+            }).then(function (response) {
+                alert(response.data.data);
+
+            });
+
+            angular.element(document.querySelector('#voteModal')).modal('hide');
+        }
+    }
+
+];
--- initial/src/main/resources/static/angular/src/scripts/index.js
+++ initial/src/main/resources/static/angular/src/scripts/index.js
@@ -7,6 +7,7 @@ myApp.controller('aktiUProceduriCtrl', require('./aktiUProceduriController.js'))
 myApp.controller('kreirajAktCtrl', require('./kreirajAktController.js'));
 myApp.controller('kreirajAmandmanCtrl', require('./kreirajAmandmanController.js'));
 myApp.controller('rukovodjenjeSednicomCtrl', require('./rukovodjenjeSednicomController.js'));
+myApp.controller('aktiUsvojeniUNaceluCtrl', require('./aktiUsvojeniUNaceluController.js'));
 myApp.controller('amandmaniCtrl', require('./amandmaniController.js'));
 myApp.service('loginService', require('./loginService.js'));
 
@@ -36,6 +37,10 @@ myApp.config(['$routeProvider',
 	when('/rukovodjenje-sednicom', {
 		templateUrl: '/angular/dist/templates/rukovodjenje-sednicom.html',
 		controller: 'rukovodjenjeSednicomCtrl'
+	}).
+	when('/akti-usvojeni-u-nacelu', {
+		templateUrl: '/angular/dist/templates/akti-usvojeni-u-nacelu.html',
+		controller: 'aktiUsvojeniUNaceluCtrl'
 	}).
 	when('/amandmani', {
 		templateUrl: '/angular/dist/templates/amandmani.html',
