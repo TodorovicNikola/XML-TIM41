caf62b90f9780a1552e26f451e6dfa852b3ccff6
vuletic <vuletic93@gmail.com>
2016-06-15 21:42:04 +0200

    StringDTO

---
+++ initial/src/main/java/xml_app/model/DTOs/StringDTO.java
@@ -0,0 +1,16 @@
+package xml_app.model.DTOs;
+
+/**
+ * Created by Vuletic on 15.6.2016.
+ */
+public class StringDTO {
+    private String am;
+
+    public String getAm() {
+        return am;
+    }
+
+    public void setAm(String am) {
+        this.am = am;
+    }
+}
